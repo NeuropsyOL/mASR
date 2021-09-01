92cd82c0fffdd90385949312ff2fa581eb0ed666
Paul Maanen <paul.maanen@uni-oldenburg.de>
2021-09-01 14:09:32 +0200

    Actually create the package

--- make.m
+++ make.m
@@ -39,4 +39,5 @@ ARGS{2}{3} = coder.cstructname(ARGS{2}{3},'asr_state_t');
 
 %% Invoke MATLAB Coder.
 codegen -config cfg asr_calibrate_simple -args ARGS{1} asr_process_simple -args ARGS{2}
-
+buildInfo=load('codegen/lib/asr/buildInfo.mat');
+packNGo(buildInfo.buildInfo,'fileName','asr.zip');
