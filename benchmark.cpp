9c1ee6d601a4388b179fff1b77b6f726c9dbe84a
Paul Maanen <paul.maanen@uni-oldenburg.de>
2021-09-24 13:39:07 +0200

    Compare complete reinitialization, resizing and preallocation of lookahead

--- benchmark.cpp
+++ benchmark.cpp
@@ -2,6 +2,7 @@
 #include "asr_calibrate_simple.h"
 #include "asr_process_simple.h"
 #include "asr_terminate.h"
+#include <iostream>
 #include <random>
 #include <optional>
 // Function Declarations
@@ -122,8 +123,38 @@ static void BM_asr_calibrate_simple(benchmark::State& state) {
     asr_calibrate_simple(X, 100, M,T,B,A,iirstate);
   }
 }
-
 static void BM_asr_process_simple(benchmark::State& state) {
+  auto X = argInit_UnboundedxUnbounded_real_T(20,50000);
+  asr_state_t instate;
+  argInit_asr_state_t(&instate);
+  double A[9];
+  double B[9];
+  asr_calibrate_simple(X, 100, instate.M,instate.T,B,A,instate.iir);
+  for(int i=0;i<9;i++){
+    instate.A[i]=A[i];
+    instate.B[i]=B[i];
+  }
+  asr_state_t outstate;
+  argInit_asr_state_t(&outstate);
+  coder::array<double, 2U> indata;
+  coder::array<double, 2U> outdata;
+  indata=argInit_UnboundedxUnbounded_real_T(20,state.range(0));
+  outdata=argInit_UnboundedxUnbounded_real_T(20,state.range(0),0);
+
+  for (auto _ : state) {
+    // Need to reset size every time, for some reason the generated
+    // code appends to indata on every call
+    indata=argInit_UnboundedxUnbounded_real_T(20,state.range(0));
+    outdata=argInit_UnboundedxUnbounded_real_T(20,state.range(0));
+    //indata.set_size(20,state.range(0));
+    //outdata.set_size(20,state.range(0));
+    asr_process_simple(indata, 100, &instate, outdata, &outstate);
+    //std::cerr<<indata.size(0)<<"x"<<indata.size(1)<<" ";
+    //std::cerr<<outdata.size(0)<<"x"<<outdata.size(1)<<"\n";
+  }
+}
+
+static void BM_asr_process_simple_norealloc(benchmark::State& state) {
   auto X = argInit_UnboundedxUnbounded_real_T(20,50000);
   asr_state_t instate;
   argInit_asr_state_t(&instate);
@@ -149,8 +180,38 @@ static void BM_asr_process_simple(benchmark::State& state) {
     asr_process_simple(indata, 100, &instate, outdata, &outstate);
   }
 }
+static void BM_asr_process_simple_norealloc_reserve_lookahead(benchmark::State& state) {
+  auto X = argInit_UnboundedxUnbounded_real_T(20,50000);
+  asr_state_t instate;
+  argInit_asr_state_t(&instate);
+  double A[9];
+  double B[9];
+  asr_calibrate_simple(X, 100, instate.M,instate.T,B,A,instate.iir);
+  for(int i=0;i<9;i++){
+    instate.A[i]=A[i];
+    instate.B[i]=B[i];
+  }
+  asr_state_t outstate;
+  argInit_asr_state_t(&outstate);
+  coder::array<double, 2U> indata;
+  coder::array<double, 2U> outdata;
+  indata=argInit_UnboundedxUnbounded_real_T(20,state.range(0));
+  outdata=argInit_UnboundedxUnbounded_real_T(20,state.range(0),0);
+  // Reserve additional space for lookahead
+  indata.set_size(20,state.range(0)+15);
+  for (auto _ : state) {
+    // Need to reset size every time, for some reason the generated
+    // code appends to indata on every call
+    indata.set_size(20,state.range(0));
+    outdata.set_size(20,state.range(0));
+    asr_process_simple(indata, 100, &instate, outdata, &outstate);
+  }
+}
+
 //Register the function as a benchmark, use milliseconds as time unit
-BENCHMARK(BM_asr_calibrate_simple)->Unit(benchmark::kMillisecond)->Range(10000,50000);
-BENCHMARK(BM_asr_process_simple)->Unit(benchmark::kMicrosecond)->DenseRange(1e2,1000,100);
+BENCHMARK(BM_asr_calibrate_simple)->Unit(benchmark::kMillisecond)->Range(1e4,1e4);
+BENCHMARK(BM_asr_process_simple)->Unit(benchmark::kMicrosecond)->DenseRange(5e1,5e2,1e2);
+BENCHMARK(BM_asr_process_simple_norealloc)->Unit(benchmark::kMicrosecond)->DenseRange(5e1,5e2,1e2);
+BENCHMARK(BM_asr_process_simple_norealloc_reserve_lookahead)->Unit(benchmark::kMicrosecond)->DenseRange(5e1,5e2,1e2);
 // Run the benchmark
 BENCHMARK_MAIN();
