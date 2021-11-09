e7e3cc2918b7999e182dacd8324d38124e9018d5
Paul Maanen <paul.maanen@uni-oldenburg.de>
2021-11-09 16:37:26 +0100

    Remove debug output

--- write_reference_out.m
+++ write_reference_out.m
@@ -8,7 +8,5 @@ state = struct('M',M,'T',T,'B',B,'A',A,'cov',[],'carry',[],'iir',iirstate,'last_
 for i=2:size(infiles,1)
     indata=load(infiles(i,:));
     [outdata,~] = asr_process_simple(indata,250,state);  % apply the processing to the data and update the filter state (as in MATLAB's filter())
-    fprintf("%ix%i -> %ix%i %.5f \n",size(indata,1),size(indata,2),size(outdata,1),size(outdata,2),outdata(1,1));
-    
     writematrix(outdata,[infiles(i,1:end-4) '.out.csv']);
 end
