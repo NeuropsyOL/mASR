38e709fa346f2b214cdb8fc453622f0db80db1db
Paul Maanen <paul.maanen@gmail.com>
2022-03-10 15:24:50 +0100

    Adapt write_reference_out to new test data

--- write_reference_out.m
+++ write_reference_out.m
@@ -1,12 +1,11 @@
-infiles=['calib_csv/sme_1_1.xdf_filt.csv'; 'calib_csv/sme_1_2.xdf_filt.csv'; 'calib_csv/sme_1_3.xdf_filt.csv'; 'calib_csv/sme_1_4.xdf_filt.csv'; 'calib_csv/sme_1_5.xdf_filt.csv';'calib_csv/sme_1_6.xdf_filt.csv';
-    'calib_csv/sme_2_1.xdf_filt.csv'; 'calib_csv/sme_2_2.xdf_filt.csv'; 'calib_csv/sme_2_3.xdf_filt.csv'; 'calib_csv/sme_2_4.xdf_filt.csv'; 'calib_csv/sme_2_5.xdf_filt.csv'; 'calib_csv/sme_2_6.xdf_filt.csv';
-    'calib_csv/sme_3_1.xdf_filt.csv'; 'calib_csv/sme_3_2.xdf_filt.csv'; 'calib_csv/sme_3_3.xdf_filt.csv'; 'calib_csv/sme_3_4.xdf_filt.csv'; 'calib_csv/sme_3_5.xdf_filt.csv'; 'calib_csv/sme_4_1.xdf_filt.csv';
-    'calib_csv/sme_4_2.xdf_filt.csv'; 'calib_csv/sme_4_3.xdf_filt.csv'];
-calibdata = load(infiles(1,:));
-[M,T,B,A, iirstate]=asr_calibrate_simple(calibdata,250);
+calibdata = load('mASR-testdata/calib.csv');
+[M,T,B,A, iirstate]=asr_calibrate_simple(calibdata,500);
 state = struct('M',M,'T',T,'B',B,'A',A,'cov',[],'carry',[],'iir',iirstate,'last_R',[],'last_trivial',true);
-for i=2:size(infiles,1)
-    indata=load(infiles(i,:));
-    [outdata,~] = asr_process_simple(indata,250,state);  % apply the processing to the data and update the filter state (as in MATLAB's filter())
-    writematrix(outdata,[infiles(i,1:end-4) '.out.csv']);
-end
+writematrix(M,'mASR-testdata/M.csv');
+writematrix(T,'mASR-testdata/T.csv');
+writematrix(B,'mASR-testdata/B.csv');
+writematrix(A,'mASR-testdata/A.csv');
+writematrix(iirstate,'mASR-testdata/iirstate.csv');
+indata=load('mASR-testdata/proc.csv');
+[outdata,~] = asr_process_simple(indata,500,state);  % apply the processing to the data and update the filter state (as in MATLAB's filter())
+writematrix(outdata, 'mASR-testdata/proc.out.csv');
