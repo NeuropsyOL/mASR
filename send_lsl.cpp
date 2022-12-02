ea3ead9dbbb2af4ccd0c88d83f30d66f3b852bd7
Paul Maanen <paul.maanen@gmail.com>
2022-12-02 16:56:31 +0100

    Add comments to send_lsl example, Use better default for input file

--- send_lsl.cpp
+++ send_lsl.cpp
@@ -1,41 +1,47 @@
 #include "lsl_cpp.h"
 #include "rapidcsv/rapidcsv.h"
-#include <thread>         // std::this_thread::sleep_for
-#include <chrono>         // std::chrono::seconds
+#include <chrono> // std::chrono::seconds
+#include <thread> // std::this_thread::sleep_for
 
-/**
- * This is a minimal lsl example that demonstrates how a multi-channel
- * stream can be used to push data to the network via stream outlet.
- * It creates a stream outlet, waits until there are one or more consumers
- * and then sends random data to the stream.
+/** This code is adapted from the LSL example code and is intended to send
+ * test data over the network via LSL for the asr plugin to receive it as
+ * "live" data.
+ * Usage: send_lsl [STREAM_NAME] [DATA_SOURCE]
+ * STREAM_NAME: Name of the LSL stream. Default: SimpleStream
+ * DATA_SOURCE: File name of the input file. send_lsl expects a comma separated table
+ * where each row represents an EEG channel
  */
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
 
   // Read data from file
-  std::string filename=argc > 2 ? argv[2] : "mASR-testdata/calib.csv";
-  auto data=rapidcsv::Document(filename,rapidcsv::LabelParams(-1,-1));
-
-  // create a new stream_info and open an outlet with it - Take name from cmd line or use
-  // "SimpleStream" as default
-  lsl::stream_info info(argc > 1 ? argv[1] : "SimpleStream", "EEG", data.GetRowCount());
+  std::string filename = argc > 2 ? argv[2] : "mASR-testdata/proc.csv";
+  auto data = rapidcsv::Document(filename, rapidcsv::LabelParams(-1, -1));
+  // create a new stream_info and open an outlet with it - Take name from cmd
+  // line or use "SimpleStream" as default
+  lsl::stream_info info(argc > 1 ? argv[1] : "SimpleStream", "EEG",
+                        data.GetRowCount());
   lsl::stream_outlet outlet(info);
   // Wait until there is a consumer before continuing - timeout is 999s
   outlet.wait_for_consumers(999);
 
-  // allocate buffer for data
+  // buffer for table data
   std::vector<double> samples;
-  std::size_t idx=0;
-  while(outlet.have_consumers()) {
-    // Fill the buffer with data
-    samples=data.GetColumn<double>(idx++ % data.GetColumnCount());
-    // send it, using lsl_local_clock as timestamp
+  std::size_t idx = 0;
+
+  for (unsigned ii = 0; ii < data.GetColumnCount(); ++ii) {
+    samples = data.GetColumn<double>(ii);
     outlet.push_sample(samples);
-    // sleep for 10 ms to limit rate to 100 Hz to get a similar frame rate as the receiving end
-    // the real rate will be slightly below, as no compensation is done for simplicity reasons,
-    // we can expect occasional dropouts in this example.
-    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    idx++;
+    // sleep for 1 ms to limit rate to 1000 Hz
+    // the real rate will be slightly below, as no compensation for
+    // processing time is done for
+    // simplicity reasons, we can expect occasional dropouts in this example.
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
   }
-  return 0;
+  // Wait until last consumer has quit
+  while (outlet.have_consumers()) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+  }
+  return idx;
 }
- 
